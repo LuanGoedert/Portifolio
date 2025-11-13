b3171626d377b8d56a277934baf7f2697ca595c0
luan goedert <luan_goedert2013@hotmail.com>
2025-11-13 01:25:16 -0300

    adding option to send message via email

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  message = '';
+
+  sendEmailMessage() {
+    if (!this.message.trim()) return; // Ignore empty messages    
+
+    const email = "luangoedert336@gmail.com";
+    const subject = "Portfolio Contact Message";
+    const body = encodeURIComponent(this.message);
+
+    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+  }
 
   onInput(event: Event) {
     const textarea = event.target as HTMLTextAreaElement;
     textarea.style.height = 'auto';
     textarea.style.height = textarea.scrollHeight + 'px';
   }
+
   public openLink(url: string): void {
     window.open(url, '_blank');
   }
+
 }
